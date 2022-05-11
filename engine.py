99fa7b8bdf83e5a08b02a52cb481cd08e4311766
syn8228 <87139967+syn8228@users.noreply.github.com>
2022-05-11 20:33:16 +0800

    Update engine.py

--- engine.py
+++ engine.py
@@ -80,6 +80,7 @@ def evaluate(data_loader, model, device):
         # compute output
         with torch.cuda.amp.autocast():
             output = model(images)
+            print(output.size())
             loss = criterion(output, target)
 
         acc1, acc5 = accuracy(output, target, topk=(1, 5))
